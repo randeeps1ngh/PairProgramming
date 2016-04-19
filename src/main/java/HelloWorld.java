e52e0e858c2037f15692d2d998b91ae1ff9a2863
randeep <randeep@giffgaff.co.uk>
2016-04-19 10:11:53 +0100

    Adding Hello World Sample

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by randeep on 19/04/2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Pair Programming ");
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by randeep on 19/04/2016.
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void simpleTest(){
+
+    }
+}
